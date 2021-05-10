1c5acee995e44f5e9d3e3d62d0df40c27f10c558
Norman Koch <norman.koch@tu-dresden.de>
2021-05-10 15:45:03 +0200

    added db with watchtimes to select episodes that have not been watched or not in a long time

--- watch.pl
+++ watch.pl
@@ -5,6 +5,7 @@ use warnings;
 use UI::Dialog;
 use Capture::Tiny ':all';
 use Data::Dumper;
+use Tie::File;
 
 my $d = new UI::Dialog ( backtitle => 'SerienWatcher', title => 'SerienWatcher',
 	height => 35, width => 65 , listheight => 25,
@@ -12,6 +13,7 @@ my $d = new UI::Dialog ( backtitle => 'SerienWatcher', title => 'SerienWatcher',
 
 my $nas_dir = "$ENV{HOME}/mailserver";
 my $seriendir = "$nas_dir/serien/";
+my $dbfile = "$seriendir/.db.txt";
 my $serie = shift @ARGV // $d->inputbox( text => "Serienname:" );
 my $staffel = shift @ARGV // '';
 my $episode = shift @ARGV // '';
@@ -88,11 +90,15 @@ sub main {
 			if($episode) {
 				$episode_file = "$staffel_ordner/".([grep { /^0+$episode\s/ } @folgen]->[0]);
 			} else {
-				$episode_file = qq#"$staffel_ordner/#.splice(@folgen, rand @folgen, 1).q#"#;
+				@folgen = sort { get_time_priorisation(qq#$staffel_ordner/$b#) <=> get_time_priorisation(qq#$staffel_ordner/$a#) || rand() <=> rand() } sort { rand() <=> rand() } @folgen;
+				#die Dumper @folgen;
+				warn Dumper +{map { $_ => get_time_priorisation("$staffel_ordner/$_") } @folgen};
+				$episode_file = qq#"$staffel_ordner/#.$folgen[0].q#"#;
+				print "Chose $episode_file with prio ".get_time_priorisation(qq#$episode_file#)."\n";
+				#die;
 			}
 
 			my @args = (qq#vlc --no-random --play-and-exit $episode_file /dev/NONEXISTANTFILE#);
-			print Dumper @args;
 
 			my ($stdout, $stderr, $exit) = capture {
 				system(@args);
@@ -101,6 +107,7 @@ sub main {
 			print $stderr;
 
 			if($stderr =~ m#NONEXISTANTFILE#) {
+				add_to_db($episode_file);
 				main();
 			} else {
 				exit;
@@ -110,6 +117,65 @@ sub main {
 		}
 	} else {
 		die "$nas_dir not found";
+	}
+}
+
+sub get_time_priorisation {
+	my $episode_file = shift;
+	$episode_file =~ s#"##g;
+	
+	my @db = ();
+	system("touch $dbfile");
+
+	tie @db, 'Tie::File', $dbfile or die "Error accessing the file $dbfile: $!"; 
+	my $prio = 1000;
+	my $found = 0;
+	foreach my $i (0 .. $#db) {
+		last if $found;
+		my $line = $db[$i];
+		if($line =~ m#(.*):::(.*)#) {
+			my ($filename, $time) = ($1, $2);
+			if($filename eq qq#"$episode_file"#) {
+				my $current_time = scalar time();
+				my $watched_days_ago = int(($current_time - $time) / 86400);
+				$prio = $watched_days_ago;
+				$found = 1;
+			}
+		}
+	}
+
+	#die "$episode_file not found" unless $found;
+
+	return $prio;
+}
+
+sub add_to_db {
+	my $episode_file = shift;
+
+	my @db = ();
+	system("touch $dbfile");
+
+	tie @db, 'Tie::File', $dbfile or die "Error accessing the file $dbfile: $!"; 
+	my $found = 0;
+	my $i = 0;
+	foreach my $line (@db) {
+		last if $found;
+		if($line =~ m#(.*):::(.*)#) {
+			my ($filename, $time) = ($1, $2);
+			if($filename eq $episode_file) {
+				$found = 1;
+				$db[$i] = $episode_file.":::".(scalar time())."\n";
+			}
+		} elsif(!$line) {
+			warn "Empty line in $dbfile";
+		} elsif($line) {
+			warn "Invalid line $line in $dbfile";
+		}
+		$i++;
+	}
+
+	if(!$found) {
+		$db[$#db + 1] = $episode_file.":::".(scalar time())."\n";
 	}
 }
 
