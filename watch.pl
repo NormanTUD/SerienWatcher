94124a9e599a061a6d43bafa0f425e14db8a707a
Norman Koch <norman.koch@tu-dresden.de>
2021-08-17 22:36:57 +0200

    next item works, but n does not work anymore

--- watch.pl
+++ watch.pl
@@ -11,6 +11,7 @@ use Capture::Tiny ':all';
 use Data::Dumper;
 use Tie::File;
 use Memoize;
+use LWP::UserAgent;
 
 my %cache;
 memoize 'get_time_priorisation_staffel', SCALAR_CACHE => [HASH => \%cache];
@@ -33,8 +34,14 @@ my %options = (
 	current_file => undef,
 	dbfile => undef,
 	zufall => 0,
-	fullscreen => 1
+	fullscreen => 1,
+	controlport => 9332,
+	controlpassword => rand(),
+	controlhost => "localhost",
+	next_in_order => 0
 );
+
+my $finished_playing = 0;
 
 my $d = new UI::Dialog ( backtitle => 'SerienWatcher', title => 'SerienWatcher',
 	height => 35, width => 65 , listheight => 25,
@@ -57,6 +64,18 @@ sub debug ($$) {
 		warn "DEBUG ($debuglevel): ".color("blue on_white").$text.color("reset")."\n";
 	}
 }
+
+$SIG{USR1} = sub { 
+	debug 3, "Received USR1, setting Zufall to 0";
+	$options{zufall} = 0;
+	$options{next_in_order} = 1;
+};
+
+$SIG{USR2} = sub {
+	debug 3, "Received USR2, setting Zufall to 1";
+	$options{zufall} = 1;
+	$options{next_in_order} = 0;
+};
 
 sub input {
 	my ($text, $entry) = @_;
@@ -97,15 +116,19 @@ sub _help {
 	print <<EOF;
 Example call:
 ${green}perl watch.pl --maindir=/home/norman/mailserver/serien/ --serie=Die-Simpsons --min_staffel=1 --max_staffel=14
---debug                                                       Enable debug
---debuglevel=4                                                Level of debug messages
---noplay                                                      Disable VLC starting (only useful for debugging)
---maindir=/path/to/maindir                                    Maindir
---serie=Serienregex                                           Serienname
---staffel=1                                                   Staffel
---min_staffel=0                                               Minimal staffel to choose from (cannot be combined with --staffel)
---max_staffel=10                                              Maximal staffel to choose from (cannot be combined with --staffel)
---zufall                                                      Random Staffel
+--debug								Enable debug
+--debuglevel=4							Level of debug messages
+--controlpassword=password					Password for controlling VLC via https (default random-pw)
+--controlport=port						Port for controlling VLC via https (default 9332)
+--controlhost=IP						Port for controlling VLC via https (default localhost)
+--dont_detect_changes_in_vlc					Do not detect changes in the VLC gui (e.g. pressing or unpressing the 'random' button)
+--no_fullscreen							Don't use fullscreen automatically
+--maindir=/path/to/maindir					Maindir
+--serie=Serienregex						Serienname
+--staffel=1							Staffel
+--min_staffel=0							Minimal staffel to choose from (cannot be combined with --staffel)
+--max_staffel=10						Maximal staffel to choose from (cannot be combined with --staffel)
+--zufall							Random Staffel
 EOF
 	exit $exit;
 }
@@ -131,8 +154,14 @@ sub analyze_args {
 			$options{debug} = 1;
 		} elsif(m#^--debuglevel=(.*)$#) {
 			$options{debuglevel} = $1;
-		} elsif(m#^--noplay$#) {
-			$options{play} = 0;
+		} elsif(m#^--controlpassword=(.*)$#) {
+			$options{controlpassword} = $1;
+		} elsif(m#^--controlport=(.*)$#) {
+			$options{controlport} = $1;
+		} elsif(m#^--controlhost=(.*)$#) {
+			$options{controlhost} = $1;
+		} elsif(m#^--no_fullscreen$#) {
+			$options{fullscreen} = 0;
 		} elsif(m#^--maindir=(.*)$#) {
 			my $maindir = $1;
 			if(-d $maindir) {
@@ -253,7 +282,7 @@ sub choose_staffel {
 	}
 
 	debug 1, "Chose staffel $options{staffel}, time prio: ".get_time_priorisation_staffel("$options{seriendir}/$options{staffel}");
-	debug 2, Dumper \%options;
+	debug 6, Dumper \%options;
 
 	$options{staffeldir} = "$options{seriendir}/$options{staffel}";
 
@@ -264,7 +293,7 @@ sub choose_staffel {
 
 sub get_time_priorisation ($) {
 	my $episode_file = shift;
-	debug 0, "get_time_priorisation($episode_file)";
+	debug 5, "get_time_priorisation($episode_file)";
 	$episode_file =~ s#"##g;
 	
 	my @db = ();
@@ -339,6 +368,7 @@ sub play_media () {
 	debug 0, "play_media";
 	choose_staffel();
 	choose_random_file();
+
 	if(defined $options{current_file} && -e $options{current_file}) {
 		my $media_runtime = get_media_runtime;
 
@@ -367,45 +397,276 @@ sub play_media () {
 			$fullscreen_command = " --fullscreen ";
 		}
 
-		my $play = qq#vlc $fullscreen_command --no-random --play-and-exit $starttime_command "$options{current_file}" "/dev/NONEXISTANTFILE" "vlc://quit"#;
+		my $http_command = " --http-port $options{controlport} --http-password $options{controlpassword} --http-host $options{controlhost} ";
+
+		$finished_playing = 0;
+		my $play = qq#vlc $http_command $fullscreen_command --no-random --play-and-stop --no-repeat --no-loop $starttime_command "$options{current_file}"#;
 		debug 1, $play;
 
-		my ($stdout, $stderr, $exit) = ('', 'NONEXISTANTFILE', '');
+		my $original_pid = $$;
+		my $pid = $$;
 
-		if($options{play}) {
-			($stdout, $stderr, $exit) = capture {
-				system($play);
-			};
+		$pid = fork();
+
+		if(not $pid) {
+			system($play);
+			my $exit_system = $?;
+			my $exit = $exit_system >> 8;
+			my $signal = $exit_system & 127;
+			debug 0, "VLC Exit-Code: $exit, Signal: $signal";
 			if($exit != 0) {
 				exit $exit;
 			}
-		} else {
-			print "Press enter to continue";
-			<STDIN>;
-		}
 
-		if($stderr =~ m#NONEXISTANTFILE#) {
-			add_to_db($options{current_file});
-			main();
+			if(finished_playing()) {
+				add_to_db($options{current_file});
+			} else {
+				debug 1, "You closed the window. Exiting";
+				exit;
+			}
 		} else {
-			debug 1, "You closed the window, as the file NONEXISTANTFILE was not found in stderr. Exiting.";
-			exit;
+			sleep 3;
+
+			if($options{zufall}) {
+				set_random_on($original_pid, 1);
+			} else {
+				set_random_off($original_pid, 1);
+			}
+
+			while (1) {
+				my $randombutton_value = randombutton_is_pressed();
+				if($randombutton_value ne $options{zufall}) {
+					if($options{zufall}) {
+						set_random_off($original_pid);
+					} else {
+						set_random_on($original_pid);
+					}
+				} else {
+					debug 4, "Not changing random status";
+				}
+				sleep 1;
+				if(finished_playing()) {
+					quit_vlc();
+					goto START;
+				}
+			}
 		}
 	} else {
 		error "Invalid current file";
 	}
 }
 
+sub quit_vlc {
+	debug 0, "quit_vlc()";
+	set_http_info("pl_empty");
+	set_http_info("in_play&input=vlc://quit");
+}
+
+sub set_random_on {
+	my $original_pid = shift;
+	my $in_player = shift // 0;
+	debug 0, "set_random_on($original_pid, $in_player)";
+	kill 'USR2', $original_pid;
+	kill 'USR2', $$;
+	if($in_player && !randombutton_is_pressed()) {
+		toggle_player_random();
+	}
+}
+
+sub set_random_off {
+	my $original_pid = shift;
+	my $in_player = shift // 0;
+	debug 0, "set_random_off($original_pid, $in_player)";
+	kill 'USR1', $original_pid;
+	kill 'USR1', $$;
+	if($in_player && randombutton_is_pressed()) {
+		toggle_player_random();
+	}
+}
+
+sub add_to_playlist {
+	my $file = shift;
+	debug 0, "add_to_playlist($file)";
+	set_http_info("command=in_enqueue&input=file://$file");
+}
+
+sub set_playlist_to_default_order {
+	debug 0, "set_playlist_to_default_order()";
+	set_http_info("command=pl_sort&id=0&val=0");
+}
+
+sub toggle_player_random {
+	debug 0, "toggle_player_random()";
+	set_http_info("pl_random");
+	set_playlist_to_default_order();
+}
+
+sub set_http_info {
+	my $command = shift;
+	debug 0, "set_http_info($command)";
+	my $browser = LWP::UserAgent->new;
+	my $url = "http://$options{controlhost}:$options{controlport}/requests/status.xml?command=$command";
+	my $req =  HTTP::Request->new( GET => $url);
+	$req->authorization_basic( "", "$options{controlpassword}" );
+	debug 6, qq#curl -s -u $options{controlpassword} "$url"#;
+	my $page = $browser->request( $req );
+	if ($page->is_success) {
+		return $page->decoded_content;
+	} else {
+		warn "Could not set $command";
+	}
+}
+
+sub get_playlist {
+	debug 0, "get_playlist()";
+	my $browser = LWP::UserAgent->new;
+	my $url = "http://$options{controlhost}:$options{controlport}/requests/playlist.xml";
+	my $req =  HTTP::Request->new( GET => $url);
+	$req->authorization_basic( "", "$options{controlpassword}" );
+	debug 6, qq#curl -s -u $options{controlpassword} "$url"#;
+	my $page = $browser->request( $req );
+	if ($page->is_success) {
+		my $p = $page->decoded_content;
+		die $p;
+		return $p;
+	} else {
+		exit;
+	}
+}
+
+sub get_full_info_http {
+	debug 0, "get_full_info_http()";
+	my $browser = LWP::UserAgent->new;
+	my $url = "http://$options{controlhost}:$options{controlport}/requests/status.xml";
+	my $req =  HTTP::Request->new( GET => $url);
+	$req->authorization_basic( "", "$options{controlpassword}" );
+	debug 6, qq#curl -s -u $options{controlpassword} "$url"#;
+	my $page = $browser->request( $req );
+	if ($page->is_success) {
+		return $page->decoded_content;
+	} else {
+		exit;
+	}
+}
+
+sub finished_playing {
+	debug 0, "finished_playing()";
+
+	return $finished_playing if $finished_playing;
+
+	my $full_info = get_full_info_http();
+	warn $full_info;
+
+	if($full_info =~ m#<currentplid>(.*)</currentplid>#) {
+		if($1 eq "-1") {
+			$finished_playing = 1;
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+	return undef;
+}
+
+sub randombutton_is_pressed {
+	my $full_info = get_full_info_http();
+	warn $full_info;
+
+	if($full_info =~ m#<random>(false|true)</random>#) {
+		if($1 eq "true") {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+	return undef;
+}
+
+sub is_integer {
+	my $str = shift;
+	return 1 if $str =~ m#^\d+$#;
+	return 0;
+}
+
+sub get_episode_nr_from_filename {
+	my $filename = shift;
+	debug 0, "get_episode_nr_from_filename($filename)";
+
+	my $nr = $filename;
+	$nr =~ s#^(\d+).*#$1#g;
+
+	return $nr;
+}
+
+sub get_next_file {
+	my $staffel = shift;
+	my $last_file_nr = shift;
+	debug 0, "get_next_file($staffel, $last_file_nr)";
+
+	my $seriendir = $options{seriendir};
+
+	my $basedir = "$options{seriendir}/$staffel/";
+
+	my @files_in_seriendir_staffel = sort { get_episode_nr_from_filename($a) <=> get_episode_nr_from_filename($b) || $a cmp $b } get_subfolders_and_files(dir => $basedir, grep => sub { m#\.mp4$# });
+	my $index = undef;
+	my $i = 0;
+	GETNEXTFILEFOREACH: foreach (@files_in_seriendir_staffel) {
+		if(m#^$last_file_nr #) {
+			$index = $i;
+			last GETNEXTFILEFOREACH;
+		}
+		$i++;
+	}
+
+	if($index == $#files_in_seriendir_staffel) {
+		$staffel++;
+		$basedir = "$options{seriendir}/$staffel/";
+		if(-d "$options{seriendir}/$staffel/") {
+			my @files_in_seriendir_next_staffel = sort { get_episode_nr_from_filename($a) <=> get_episode_nr_from_filename($b) || $a cmp $b } get_subfolders_and_files(dir => $basedir, grep => sub { m#\.mp4$# });
+			$options{staffel} = $staffel;
+			$options{current_file} = $basedir.$files_in_seriendir_next_staffel[0];
+		} else {
+			$options{current_file} = undef;
+		}
+	} else {
+		$options{current_file} = $basedir.$files_in_seriendir_staffel[$index + 1];
+	}
+}
+
 sub choose_random_file {
 	debug 0, "choose_random_file()";
-	my @list = map { "$options{staffeldir}/$_" } get_subfolders_and_files(dir => $options{staffeldir}, grep => sub { m#\.mp4$# });
-	my @weight = ();
-	foreach (@list) {
-		push @weight, get_time_priorisation("$options{seriendir}/$_");
+	my $chosen_properly = 0;
+	if($options{current_file} && $options{next_in_order}) {
+		my $current_file = $options{current_file};
+		my $current_staffel_dir = $current_file;
+
+		my $staffel_nr = $current_staffel_dir;
+		$staffel_nr =~ s#.*/(\d+)/.*?$#$1#g;
+
+		my $current_filename = $current_file;
+		$current_filename =~ s#.*/(.*?)$#$1#g;
+
+		my $current_file_nr = $current_filename;
+		$current_file_nr =~ s#(\d+) -.*#$1#g;
+
+		debug 0, "=>=>=>=>=>=>=>=>=>=>=>=>=>staffelnr: $staffel_nr";
+		debug 0, ">>>>>>>>>>>>>>>>>>>>>>>>>>get_next_file($staffel_nr, $current_file_nr);";
+		get_next_file($staffel_nr, $current_file_nr);
+		if(defined $options{current_file}) {
+			$chosen_properly = 1;
+		}
 	}
-	my $thing = Math::Random::Discrete->new(\@weight, \@list);
-	$options{current_file} = $thing->rand;
-	debug 1, "Chose $options{current_file} (prio: ".get_time_priorisation("$options{current_file}").")";
+
+	if(!$chosen_properly) {
+		my @list = map { "$options{staffeldir}/$_" } get_subfolders_and_files(dir => $options{staffeldir}, grep => sub { m#\.mp4$# });
+		my @weight = ();
+		foreach (@list) {
+			push @weight, get_time_priorisation("$options{seriendir}/$_");
+		}
+		my $thing = Math::Random::Discrete->new(\@weight, \@list);
+		$options{current_file} = $thing->rand;
+		debug 1, "Chose $options{current_file} (prio: ".get_time_priorisation("$options{current_file}").")";
+	}
 }
 
 sub get_time_priorisation_staffel {
@@ -445,7 +706,7 @@ sub get_time_priorisation_staffel {
 		$sum = int($sum / scalar(@files));
 	}
 
-	debug 1, "get_time_priorisation_staffel($dir) = $sum";
+	debug 5, "get_time_priorisation_staffel($dir) = $sum";
 
 	return $sum;
 }
@@ -499,4 +760,5 @@ sub main () {
 
 analyze_args(@ARGV);
 
+START:
 main;
